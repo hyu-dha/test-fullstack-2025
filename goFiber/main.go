f6fa89deceb0004656f306523f6195c0275ffb18
Yudha <hyudhapahlawan@gmail.com>
2025-08-27 10:07:19 +0700

    feat: implement login functionality and add login.html (unfinished fogiber)

--- goFiber/main.go
+++ goFiber/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"fmt"
+	"net/http"
 )
 
-func login(c *fiber.Ctx) error {
-	type User struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-	var user User
-	if err := c.BodyParser(&user); err != nil {
-		return err
-	}
-	return c.JSON(user)
+var users = map[string]string{
+	"user1": "password123",
+	"admin": "adminpass",
 }
 
 func main() {
-	app := fiber.New()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "login.html")
+	})
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			nama := r.FormValue("nama")
+			email := r.FormValue("email")
 
-	app.Post("/", login)
+			fmt.Fprintf(w, "Nama Anda: %s, Email: %s", nama, email)
+		} else {
+			http.Error(w, "Metode tidak diizinkan", http.StatusMethodNotAllowed)
+		}
+	})
 
-	app.Listen(":3000")
+		fmt.Println("Server berjalan di http://localhost:8080")
+		http.ListenAndServe(":8080", nil)
 }
